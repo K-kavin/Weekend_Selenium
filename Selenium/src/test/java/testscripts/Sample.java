6e58e3e0c71955fafcd74c59b93e0bfad2648ce6
QSPR <QSPR@AUTOMATION-LAB>
2023-10-01 10:15:15 +0530

    Build B01

---
+++ Selenium/src/test/java/testscripts/Sample.java
@@ -0,0 +1,15 @@
+package testscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Sample 
+{
+	@Test
+	public void test()
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.amazon.in");
+	}
+}
